79f81d0b54465471043e3b5d1e748f4092d223c3
ROG\simba <ayugupta.jpr@gmail.com>
2018-11-05 06:18:44 +0530

    db config

--- src/api/config/Database.php
+++ src/api/config/Database.php
@@ -4,8 +4,8 @@ class Database{
     // specify your own database credentials
     private $host = "localhost";
     private $db_name = "inventory";
-    private $username = "root";
-    private $password = "";
+    private $username = "YOUR-DB-USERNAME-HERE";
+    private $password = "YOUR-DB-PASSWORD-HERE";
     public $conn;
  
     // get the database connection
